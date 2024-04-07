1ce6176190817e9bdcd58845818e9a4614644113
Vinicius Cruz <viniciusmdc.dev@gmail.com>
2024-04-07 11:53:43 -0300

    Initial commit

---
+++ src/main/java/com/viniciusmdc/ExemploPatternSingleton/ExemploPatternSingletonApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusmdc.ExemploPatternSingleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploPatternSingletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploPatternSingletonApplication.class, args);
+	}
+
+}
